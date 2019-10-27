4a20b92e9470e3e0b65c9601f6cdcaf9615b6aa4
wilderwil <wilderwil@gmail.com>
2019-10-26 21:20:09 -0400

    Install React y ReactDOM, creacion de componente Inicial

---
+++ components/ComponenteInicial/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Component } from "react-dom";
+
+class  ComponenteInicial extends React.Component{
+
+   render() {
+        return <div>App con React Babel y Webpack configuardo Manualmente</div>
+   }; 
+} 
+export default ComponenteInicial
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export const mensaje=()=>alert('Mensaje de prueba3');
-mensaje();
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import ComponenteInicial from '../components/ComponenteInicial/index';
+const element = <ComponenteInicial />
+ReactDOM.render(element, document.getElementById('root'));
